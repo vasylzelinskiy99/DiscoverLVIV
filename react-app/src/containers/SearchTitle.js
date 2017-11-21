3c9dc69536a50bcb4708c72d67108552fe6414cf
OlhaPolishchuk <OlhaPolishchuk@users.noreply.github.com>
2017-11-21 23:04:46 +0200

    Added title for search page

---
+++ react-app/src/containers/SearchTitle.js
@@ -0,0 +1,12 @@
+import React, {PropTypes} from 'react';
+
+export const SearchTitle = (props) => {
+  return (
+    <div className="topbanner">
+      <h2>Результати пошуку</h2>
+    </div>
+  );
+}
+
+SearchTitle.propTypes = {
+};
